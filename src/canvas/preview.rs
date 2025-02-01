f68fee9a3fc7876ec51e761a6696e378e7096377
Jala3400 <jala.3400@gmail.com>
2025-02-01 11:21:59 +0100

    Paint square when pressing ctrl

--- src/canvas/preview.rs
+++ src/canvas/preview.rs
@@ -4,16 +4,78 @@ impl Canvas {
     pub fn update_preview(&mut self) {
         match self.preview {
             Preview::Line(_, _) => self.update_line_preview(self.cursor_pos.0, self.cursor_pos.1),
+            Preview::Square(_, _) => {
+                self.update_square_preview(self.cursor_pos.0, self.cursor_pos.1)
+            }
             Preview::None => {}
         }
     }
 
+    pub fn delete_preview(&mut self) {
+        match self.preview {
+            Preview::Line(_, _) => self.delete_line_preview(),
+            Preview::Square(_, _) => self.delete_square_preview(),
+            Preview::None => {}
+        }
+    }
+
+    pub fn paint_preview(&mut self) {
+        match self.preview {
+            Preview::Line(_, _) => self.paint_line_preview(self.cursor_pos.0, self.cursor_pos.1),
+            Preview::Square(_, _) => {
+                self.paint_square_preview(self.cursor_pos.0, self.cursor_pos.1)
+            }
+            Preview::None => {}
+        }
+    }
+
+    fn paint_circle_preview(&mut self, center_x: i32, center_y: i32) {
+        let radius = self.radius as i32;
+        let mut x = radius;
+        let mut y = 0;
+        let mut decision = 1 - radius;
+
+        while x >= y {
+            self.draw_horizontal_line_preview(center_x - x, center_x + x, center_y + y);
+            self.draw_horizontal_line_preview(center_x - x, center_x + x, center_y - y);
+            self.draw_horizontal_line_preview(center_x - y, center_x + y, center_y + x);
+            self.draw_horizontal_line_preview(center_x - y, center_x + y, center_y - x);
+
+            y += 1;
+
+            if decision <= 0 {
+                decision += 2 * y + 1;
+            } else {
+                x -= 1;
+                decision += 2 * (y - x) + 1;
+            }
+        }
+    }
+
+    #[inline]
+    fn draw_horizontal_line_preview(&mut self, x1: i32, x2: i32, y: i32) {
+        let drawing_color = self.get_drawing_color();
+        if let Ok(mut buffer) = self.surface.buffer_mut() {
+            let (width, height) = self.window_size;
+            if y < 0 || y >= height {
+                return;
+            }
+
+            let start_x = (y * self.window_size.0 + x1.max(0).min(width - 1)) as usize;
+            let end_x = (y * self.window_size.0 + x2.max(0).min(width - 1)) as usize;
+
+            buffer[start_x..end_x].fill(drawing_color);
+        }
+    }
+}
+
+impl Canvas {
     pub fn update_line_preview(&mut self, x1: i32, y1: i32) {
         self.delete_line_preview();
         self.paint_line_preview(x1, y1);
     }
 
-    pub fn delete_line_preview(&mut self) {
+    fn delete_line_preview(&mut self) {
         if let Preview::Line(x0, y0) = self.preview {
             let (x1, y1) = self.cursor_pos;
             let dx = (x1 - x0).abs();
@@ -91,7 +153,7 @@ impl Canvas {
         }
     }
 
-    pub fn paint_line_preview(&mut self, x1: i32, y1: i32) {
+    fn paint_line_preview(&mut self, x1: i32, y1: i32) {
         if let Preview::Line(x0, y0) = self.preview {
             let dx = (x1 - x0).abs();
             let dy = (y1 - y0).abs();
@@ -119,43 +181,68 @@ impl Canvas {
             }
         }
     }
+}
 
-    fn paint_circle_preview(&mut self, center_x: i32, center_y: i32) {
-        let radius = self.radius as i32;
-        let mut x = radius;
-        let mut y = 0;
-        let mut decision = 1 - radius;
+impl Canvas {
+    pub fn update_square_preview(&mut self, x1: i32, y1: i32) {
+        self.delete_square_preview();
+        self.paint_square_preview(x1, y1);
+    }
 
-        while x >= y {
-            self.draw_horizontal_line_preview(center_x - x, center_x + x, center_y + y);
-            self.draw_horizontal_line_preview(center_x - x, center_x + x, center_y - y);
-            self.draw_horizontal_line_preview(center_x - y, center_x + y, center_y + x);
-            self.draw_horizontal_line_preview(center_x - y, center_x + y, center_y - x);
+    fn delete_square_preview(&mut self) {
+        if let Preview::Square(x0, y0) = self.preview {
+            let (x1, y1) = self.cursor_pos;
+            let x_min = x0.min(x1);
+            let x_max = x0.max(x1);
+            let y_min = y0.min(y1);
+            let y_max = y0.max(y1);
 
-            y += 1;
+            for x in x_min..=x_max {
+                self.delete_circle_preview(x, y_min);
+                self.delete_circle_preview(x, y_max);
+            }
 
-            if decision <= 0 {
-                decision += 2 * y + 1;
-            } else {
-                x -= 1;
-                decision += 2 * (y - x) + 1;
+            for y in y_min..=y_max {
+                self.delete_circle_preview(x_min, y);
+                self.delete_circle_preview(x_max, y);
             }
         }
     }
 
-    #[inline]
-    fn draw_horizontal_line_preview(&mut self, x1: i32, x2: i32, y: i32) {
-        let drawing_color = self.get_drawing_color();
-        if let Ok(mut buffer) = self.surface.buffer_mut() {
-            let (width, height) = self.window_size;
-            if y < 0 || y >= height {
-                return;
+    fn paint_square_preview(&mut self, x1: i32, y1: i32) {
+        if let Preview::Square(x0, y0) = self.preview {
+            let x_min = x0.min(x1);
+            let x_max = x0.max(x1);
+            let y_min = y0.min(y1);
+            let y_max = y0.max(y1);
+
+            for x in x_min..=x_max {
+                self.paint_circle_preview(x, y_min);
+                self.paint_circle_preview(x, y_max);
             }
 
-            let start_x = (y * self.window_size.0 + x1.max(0).min(width - 1)) as usize;
-            let end_x = (y * self.window_size.0 + x2.max(0).min(width - 1)) as usize;
+            for y in y_min..=y_max {
+                self.paint_circle_preview(x_min, y);
+                self.paint_circle_preview(x_max, y);
+            }
+        }
+    }
 
-            buffer[start_x..end_x].fill(drawing_color);
+    pub fn paint_square(&mut self, x1: i32, y1: i32) {
+        let (x0, y0) = self.cursor_pos;
+        let x_min = x0.min(x1);
+        let x_max = x0.max(x1);
+        let y_min = y0.min(y1);
+        let y_max = y0.max(y1);
+
+        for x in x_min..=x_max {
+            self.paint_circle(x, y_min);
+            self.paint_circle(x, y_max);
+        }
+
+        for y in y_min..=y_max {
+            self.paint_circle(x_min, y);
+            self.paint_circle(x_max, y);
         }
     }
 }
